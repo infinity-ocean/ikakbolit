867677175bd91c0f0e42fb1b0507aa96e5cd78c1
infinity-ocean <varrr7@gmail.com>
2025-05-10 00:18:45 +0200

    refactor(pkg/../modules): moved HTTP and gRPC servers start to modules package

--- pkg/application/modules/grpc_server.go
+++ pkg/application/modules/grpc_server.go
@@ -3,21 +3,18 @@ package modules
 import (
 	"log/slog"
 	"os"
-	"strconv"
 
 	"github.com/infinity-ocean/ikakbolit/internal/config"
 	"github.com/infinity-ocean/ikakbolit/internal/server/grpc"
 )
 
-type GRPCServer struct {}
-
-func (h GRPCServer) Run(
+func RunGRPC(
 	grpc *grpc.GRPCServer,
 	cfg config.Config,
 	log *slog.Logger,
 ) {
 	go func() {
-		log.Info("Starting gRPC server", "address", strconv.Itoa(cfg.GRPC.ListenAddress))
+		log.Info("Starting gRPC server", "address", cfg.GRPC.Port)
 		if err := grpc.Run(); err != nil {
 			log.Error("gRPC server error:", "err", err)
 			os.Exit(1)
--- pkg/application/modules/http_server.go
+++ pkg/application/modules/http_server.go
@@ -1,26 +1,33 @@
 package modules
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log/slog"
-	"os"
-	"strconv"
+	"net/http"
 
-	"github.com/infinity-ocean/ikakbolit/internal/config"
 	"github.com/infinity-ocean/ikakbolit/internal/server/rest"
+	"golang.org/x/sync/errgroup"
 )
 
-type HTTPServer struct {}
+type HTTPServer struct {
+	Port   string
+	Router *rest.HTTPRouter
+	Log    *slog.Logger
+}
 
 func (h HTTPServer) Run(
-	rest *rest.HTTPServer,
-	cfg config.Config,
-	log *slog.Logger,
+	_ context.Context,
+	g *errgroup.Group,
 ) {
-	go func() {
-			log.Info("Starting REST server", "address", strconv.Itoa(cfg.HTTP.ListenAddress))
-			if err := rest.Run(); err != nil {
-				log.Error("REST server error:", "err", err)
-				os.Exit(1)
+	g.Go(func() error {
+		h.Log.Info("Starting HTTP server", "port", h.Port)
+		if err := http.ListenAndServe(h.Port, h.Router.GetRouter()); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("http.ListenAndServe: %w", err)
 		}
-		}()
+		h.Log.Info("http server stopped")
+
+		return nil
+	})
 }
